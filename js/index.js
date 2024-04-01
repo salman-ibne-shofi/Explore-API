ba012860fff34fbc2420ddbfad12b420a3cfb1d9
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-04-02 02:15:29 +0600

    created a file hooked up with index html file and use to learn dynamically display loaded data on my website

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 function loadUsers2() {
-	fetch("https://jsonplaceholder.typicode.com/")
+	fetch("https://jsonplaceholder.typicode.com/users")
 		.then((res) => res.json())
-		.then((data) => console.log(data));
+		.then((data) => displayUsers2(data));
+}
+
+function displayUsers2(data) {
+	const ul = document.getElementById("users-list");
+	for (const user of data) {
+		console.log(user.name);
+		const li = document.createElement("li");
+		li.innerText = user.name;
+		ul.appendChild(li);
+	}
 }
